b51e82bbc51f8dc38c1cf462f18a4839d3721ce3
Jan Tojnar <jtojnar@gmail.com>
2026-08-03 11:42:53 +0200

    composer: Add PHP-CS-Fixer scripts
    This will allow checking/fixing coding style violations locally.

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+return (new Config())
+    ->setRiskyAllowed(false)
+    ->setRules([
+        '@auto' => true,
+        '@Symfony' => true,
+    ])
+    ->setFinder(
+        (new Finder())
+            ->in(__DIR__)
+    )
+;
